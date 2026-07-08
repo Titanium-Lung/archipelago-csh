f1db3acbc7d0cce9c5733f01bd266c8a50d52184
Titanium-Lung <cooper@psychofx.com>
2026-07-07 18:32:03 -0700

    Bold and pink hint colors

--- frontend/src/pages/Tracker.jsx
+++ frontend/src/pages/Tracker.jsx
@@ -328,8 +328,20 @@ function Tracker() {
                     <tbody>
                         {sortedHints.map((hint, index) => (
                             <tr key={index}>
-                                <td>{hint.finding_player}</td>
-                                <td>{hint.receiving_player}</td>
+                                {
+                                    slotName === hint.finding_player ? (
+                                        <td className="individual-hint-player">{hint.finding_player}</td>
+                                    ) : (
+                                        <td>{hint.finding_player}</td>
+                                    )
+                                }
+                                {
+                                    slotName === hint.receiving_player ? (
+                                        <td className="individual-hint-player">{hint.receiving_player}</td>
+                                    ) : (
+                                        <td>{hint.receiving_player}</td>
+                                    )
+                                }
                                 <td>{hint.item}</td>
                                 <td>{hint.location}</td>
                                 <td>{hint.game}</td>
